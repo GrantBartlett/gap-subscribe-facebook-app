ef1e6c66b51486acf9ab038319fcb366e9c4c0bf
Grant Bartlett <grant@smswmedia.com>
2015-04-16 09:44:04 +0100

    Update to include mobile field

--- index.php
+++ index.php
@@ -1,69 +1,73 @@
 <?php
-    require_once('_include/Language.php');
-    require_once('_include/Strings.php');
-    $locale = new Language();
-    $strings = new Strings();
-    $contentArr = $strings->getStrings();
-    $contentArr = $contentArr[$locale->getLanguage()];
+require_once( '_include/Language.php' );
+require_once( '_include/Strings.php' );
+$locale     = new Language();
+$strings    = new Strings();
+$contentArr = $strings->getStrings();
+$contentArr = $contentArr[ $locale->getLanguage() ];
 ?>
 
-<?php include_once('_include/header.php'); ?>
+<?php include_once( '_include/header.php' ); ?>
 
-<body <?php if($locale->getLanguage() == 'ar_AR') echo 'class="rtl"';?> >
+<body <?php if ( $locale->getLanguage() == 'ar_AR' ) { echo 'class="rtl"'; } ?>>
 
 <div class="container-fluid">
-    <section class="app feature" role="main">
+	<section class="app feature" role="main">
 
-        <div class="row header">
-            <div class="col-md-12">
+		<div class="row header">
+			<div class="col-md-12">
 
-                <div class="main">
+				<div class="main">
 
-	                <section class="logo">
-		                <img src="dist/images/logo.jpg" width="91" height="91" alt="GAP logo" class="img-responsive">
-	                </section>
+					<section class="logo">
+						<img src="dist/images/logo.jpg" width="91" height="91" alt="GAP logo" class="img-responsive">
+					</section>
 
-                    <?php foreach($contentArr as $string): ?>
-	                <section class="intro">
-		                <img src="dist/images/headline.jpg" alt="Be in the know" width="395" height="45" class="img-responsive">
-		                <br>
+					<?php foreach ( $contentArr as $string ): ?>
+						<section class="intro">
+							<img src="dist/images/headline.jpg" alt="Be in the know" width="395" height="45" class="img-responsive">
+							<br>
 
-		                <p>
-							<?php echo $string['intro']['text']; ?>
-		                </p>
-	                </section>
+							<p>
+								<?php echo $string['intro']['text']; ?>
+							</p>
+						</section>
 
-                    <section class="form">
-                        <div class="row">
-                            <div class="col-md-12">
+						<section class="form">
+							<div class="row">
+								<div class="col-md-12">
 
-                                <form action="https://smswmedia.createsend.com/t/d/s/iyithu/" method="post" id="form">
-                                    <div class="form-group">
-                                        <label for="fieldEmail"><?php echo $string['form']['email']; ?></label><br/>
-                                        <input id="fieldEmail" name="cm-iyithu-iyithu" type="email" data-parsley-type="email" data-parsley-required-message="<?php echo $string['validate']['invalid']; ?>" class="form-control" placeholder="<?php echo $string['form']['email-placeholder']; ?>" required>
-                                    </div>
+									<form action="https://smswmedia.createsend.com/t/d/s/iyithu/" method="post" id="form">
+										<div class="form-group">
+											<label for="fieldEmail"><?php echo $string['form']['email']; ?></label><br/>
+											<input id="fieldEmail" name="cm-iyithu-iyithu" type="email" data-parsley-type="email" data-parsley-required-message="<?php echo $string['validate']['invalid']; ?>" class="form-control" placeholder="<?php echo $string['form']['email-placeholder']; ?>" required>
+										</div>
 
-                                    <div class="form-group">
-                                        <label for="confirmEmail"><?php echo $string['form']['email-confirm']; ?></label><br/>
-                                        <input id="confirmEmail" data-parsley-equalto="#fieldEmail" type="email" data-parsley-type="email" data-parsley-required-message="<?php echo $string['validate']['confirm']; ?>" class="form-control" placeholder="<?php echo $string['form']['email-confirm']; ?>" required>
-                                    </div>
+										<div class="form-group">
+											<label for="confirmEmail"><?php echo $string['form']['email-confirm']; ?></label><br/>
+											<input id="confirmEmail" data-parsley-equalto="#fieldEmail" type="email" data-parsley-type="email" data-parsley-required-message="<?php echo $string['validate']['confirm']; ?>" class="form-control" placeholder="<?php echo $string['form']['email-confirm']; ?>" required>
+										</div>
 
-                                    <p>
-                                        <button type="submit" class="btn btn-primary text-uppercase"><?php echo $string['form']['submit']; ?>
-                                            <i class="fa fa-play"></i>
-                                        </button>
-                                    </p>
-                                </form>
+										<div class="form-group">
+											<label for="fieldjilrdu"><?php echo $string['form']['mobile']; ?></label><br/>
+											<input id="fieldjilrdu" name="cm-f-jilrdu" type="text" data-parsley-type="number" data-parsley-required-message="<?php echo $string['validate']['invalid-mobile']; ?>" class="form-control" placeholder="<?php echo $string['form']['mobile-placeholder']; ?>" required>
+										</div>
+										<p>
+											<button type="submit" class="btn btn-primary text-uppercase"><?php echo $string['form']['submit']; ?>
+												<i class="fa fa-play"></i>
+											</button>
+										</p>
+									</form>
 
-                            </div>
-                        </div>
-                    </section>
-                    <?php endforeach; ?>
+								</div>
+							</div>
+						</section>
+					<?php endforeach; ?>
 
-                </div>
-            </div>
-        </div>
-    </section>
+				</div>
+			</div>
+		</div>
+	</section>
 </div>
 
-<?php include_once('_include/footer.php'); ?>
+<?php include_once( '_include/footer.php' ); ?>
